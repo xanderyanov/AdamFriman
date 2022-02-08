19e16e78b59ada97c94711aea7194d449381bf07
xanderyanov <xanderyanov@gmail.com>
2022-02-08 17:46:33 +0400

    17-46

--- 01/FirstProject/Controllers/HomeController.cs
+++ 01/FirstProject/Controllers/HomeController.cs
@@ -1,17 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FirstProject.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 
 namespace FirstProject.Controllers
 {
+
+    
+
     public class HomeController : Controller
     {
         public ViewResult Index()
         {
             int hour = DateTime.Now.Hour;
-            
-            string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
-            
-            return View("MyView", viewModel);
+
+            //string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+
+            Person people = new Person
+            {
+                name = "John",
+                age = 25,
+            };
+
+            return View("MyView", people);
 
         }
 
